9ed36ea58526ac57e82ee9d3f1900e7c1715344a
Hoe Hooi Yan <hoecoding@gmail.com>
2020-07-11 19:17:05 +0800

    Initial commit

---
+++ 03-expressions/exercise-3.9.scm
@@ -0,0 +1,21 @@
+#lang simply-scheme
+
+; Exercise 3.9
+; The expression (+ 8 2) has the value 10. It is a compound expression made up of three atoms.
+; For this problem, write five other Scheme expressions whose values are also the number ten:
+
+; - An atom
+10
+
+; - Another compound expression made up of three atoms
+(+ 6 4)
+
+; - A compound expression made up of four atoms
+(+ 5 3 2)
+
+; - A compound expression made up of an atom and two compound subexpressions
+(+ (+ 2 2) (- 10 4))
+
+; - Any other kind of expression
+(* 5 2)
+(/ 20 2)
